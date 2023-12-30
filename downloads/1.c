dfbf485552fae9e3a54ddaeda1476b05fe73d858
41223146 <144768158+41223146@users.noreply.github.com>
2023-12-30 09:28:42 +0000

    新增ANSIC頁面

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 蔡秉延\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : August 29, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0931-819089\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
